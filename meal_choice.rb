b45f804efbccbf4d12c9754dec64abc310d8d13e
KarinaMealey <kem32002@gmail.com>
2018-06-19 15:37:29 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -26,6 +26,6 @@ puts snacks
 def meal_choice(breakfast="frosted flakes", lunch="grilled cheese", dinner="salmon")
   puts "Morning is the best time for #{breakfast}! I love to eat #{lunch} for lunch and #{dinner} for dinner!"
 end
-puts meal_choice("pancakes", "sandwiches", "tacos")
+puts #meal_choice("pancakes", "sandwiches", "tacos")
 
 puts meal_choice
